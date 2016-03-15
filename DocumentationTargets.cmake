a3c1f7d985614218febc350f94c7ea61d4102fa4
Iwer Petersen <iwer.petersen@haw-hamburg.de>
2016-03-16 00:28:09 +0100

    Better named Doxygen Targets

--- DocumentationTargets.cmake
+++ DocumentationTargets.cmake
@@ -8,30 +8,30 @@ FIND_PACKAGE (Doxygen)
 IF (DOXYGEN_FOUND)
     IF (EXISTS ${PROJECT_SOURCE_DIR}/Doxyfile)
         ADD_CUSTOM_TARGET(
-            doxygen
+            ${PROJECT_NAME}-doxygen
             ${DOXYGEN_EXECUTABLE} Doxyfile
             WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
             COMMENT "Generating doxygen project documentation." VERBATIM
         )
         ADD_CUSTOM_TARGET(
-            documentation
+            ${PROJECT_NAME}-documentation
             ${DOXYGEN_EXECUTABLE} Doxyfile
             WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
             COMMENT "Generating doxygen project documentation." VERBATIM
         )
         ADD_CUSTOM_TARGET(
-            docs
+            ${PROJECT_NAME}-docs
             ${DOXYGEN_EXECUTABLE} Doxyfile
             WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
             COMMENT "Generating doxygen project documentation." VERBATIM
         )
     ELSE (EXISTS ${PROJECT_SOURCE_DIR}/Doxyfile)
-        ADD_CUSTOM_TARGET(doxygen COMMENT "Doxyfile not found. Please generate a doxygen configuration file to use this target." VERBATIM)
-        ADD_CUSTOM_TARGET(documentation COMMENT "Doxyfile not found. Please generate a doxygen configuration file to use this target." VERBATIM)
-        ADD_CUSTOM_TARGET(docs COMMENT "Doxyfile not found. Please generate a doxygen configuration file to use this target." VERBATIM)
+        ADD_CUSTOM_TARGET(${PROJECT_NAME}-doxygen COMMENT "Doxyfile not found. Please generate a doxygen configuration file to use this target." VERBATIM)
+        ADD_CUSTOM_TARGET(${PROJECT_NAME}-documentation COMMENT "Doxyfile not found. Please generate a doxygen configuration file to use this target." VERBATIM)
+        ADD_CUSTOM_TARGET(${PROJECT_NAME}-docs COMMENT "Doxyfile not found. Please generate a doxygen configuration file to use this target." VERBATIM)
     ENDIF (EXISTS ${PROJECT_SOURCE_DIR}/Doxyfile)
 ELSE (DOXYGEN_FOUND)
-    ADD_CUSTOM_TARGET(doxygen COMMENT "Doxygen not found. Please install doxygen to use this target." VERBATIM)
-    ADD_CUSTOM_TARGET(documentation COMMENT "Doxygen not found. Please install doxygen to use this target." VERBATIM)
-    ADD_CUSTOM_TARGET(docs COMMENT "Doxygen not found. Please install doxygen to use this target." VERBATIM)
+    ADD_CUSTOM_TARGET(${PROJECT_NAME}-doxygen COMMENT "Doxygen not found. Please install doxygen to use this target." VERBATIM)
+    ADD_CUSTOM_TARGET(${PROJECT_NAME}-documentation COMMENT "Doxygen not found. Please install doxygen to use this target." VERBATIM)
+    ADD_CUSTOM_TARGET(${PROJECT_NAME}-docs COMMENT "Doxygen not found. Please install doxygen to use this target." VERBATIM)
 ENDIF (DOXYGEN_FOUND)
