fe59431795ba5be17108b11836361edeb184d748
Iwer Petersen <iwer.petersen@haw-hamburg.de>
2016-03-20 19:03:11 +0100

    Also find Unittestpp debug lib. Added Target definition include files for Boost and PCL

--- FindUnitTestPlusPlus.cmake
+++ FindUnitTestPlusPlus.cmake
@@ -5,20 +5,28 @@
 
 SET(unittestplusplus_DIR "" CACHE PATH "Root path of UnitTest++")
 
-FIND_PATH(UnitTestPlusPlus_INCLUDE_DIR NAMES unittest++/UnitTest++.h)
+FIND_PATH(UnitTestPlusPlus_INCLUDE_DIR NAMES unittest++/UnitTest++.h
+	HINTS ${unittestplusplus_DIR}/include)
 MARK_AS_ADVANCED(UnitTestPlusPlus_INCLUDE_DIR)
 
-FIND_LIBRARY(UnitTestPlusPlus_LIBRARY NAMES UnitTest++)
+FIND_LIBRARY(UnitTestPlusPlus_LIBRARY NAMES UnitTest++
+	HINTS ${unittestplusplus_DIR}/lib)
 MARK_AS_ADVANCED(UnitTestPlusPlus_LIBRARY)
 
+FIND_LIBRARY(UnitTestPlusPlus_LIBRARY_DEBUG NAMES UnitTest++d
+	HINTS ${unittestplusplus_DIR}/lib)
+MARK_AS_ADVANCED(UnitTestPlusPlus_LIBRARY_DEBUG)
+
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(UnitTestPlusPlus DEFAULT_MSG UnitTestPlusPlus_LIBRARY UnitTestPlusPlus_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(UnitTestPlusPlus DEFAULT_MSG UnitTestPlusPlus_LIBRARY UnitTestPlusPlus_LIBRARY_DEBUG UnitTestPlusPlus_INCLUDE_DIR)
 
 IF ("${UnitTestPlusPlus_INCLUDE_DIR}" MATCHES "NOTFOUND")
     SET (UnitTestPlusPlus_LIBRARY)
+	SET (UnitTestPlusPlus_LIBRARY_DEBUG)
     SET (UnitTestPlusPlus_INCLUDE_DIR)
 ELSEIF ("${UnitTestPlusPlus_LIBRARY}" MATCHES "NOTFOUND")
     SET (UnitTestPlusPlus_LIBRARY)
+	SET (UnitTestPlusPlus_LIBRARY_DEBUG)
     SET (UnitTestPlusPLus_INCLUDE_DIR)
 ELSE ("${UnitTestPlusPlus_INCLUDE_DIR}" MATCHES "NOTFOUND")
     SET (UnitTestPlusPlus_FOUND 1)
---
+++ TargetsBoostStatic.cmake
@@ -0,0 +1,61 @@
+if(WIN32)
+	SET(Boost_COMPILER "-vc140")
+endif()
+set(Boost_ADDITIONAL_VERSIONS "1.47" "1.47.0" "1.60" "1.60.0")
+find_package(Boost COMPONENTS "chrono" "date_time" "filesystem" "graph" "iostreams" "program_options" "regex" "serialization" "signals" "system" "thread" "wserialization" REQUIRED)
+#LINK_DIRECTORIES(${Boost_LIBRARY_DIRS})
+add_library(boost-datetime STATIC IMPORTED)
+set_property(TARGET boost-datetime PROPERTY
+    INTERFACE_INCLUDE_DIRECTORIES ${Boost_INCLUDE_DIRS})
+set_property(TARGET boost-datetime PROPERTY 
+	IMPORTED_LOCATION_RELEASE ${Boost_DATE_TIME_LIBRARY_RELEASE})
+set_property(TARGET boost-datetime PROPERTY 
+	IMPORTED_LOCATION_DEBUG ${Boost_DATE_TIME_LIBRARY_DEBUG})
+
+add_library(boost-filesystem STATIC IMPORTED)
+set_property(TARGET boost-filesystem PROPERTY
+    INTERFACE_INCLUDE_DIRECTORIES ${Boost_INCLUDE_DIRS})
+set_property(TARGET boost-filesystem PROPERTY 
+	IMPORTED_LOCATION_RELEASE ${Boost_FILESYSTEM_LIBRARY_RELEASE})
+set_property(TARGET boost-filesystem PROPERTY 
+	IMPORTED_LOCATION_DEBUG ${Boost_FILESYSTEM_LIBRARY_DEBUG})
+
+add_library(boost-system STATIC IMPORTED)
+set_property(TARGET boost-system PROPERTY
+    INTERFACE_INCLUDE_DIRECTORIES ${Boost_INCLUDE_DIRS})
+set_property(TARGET boost-system PROPERTY 
+	IMPORTED_LOCATION_RELEASE ${Boost_SYSTEM_LIBRARY_RELEASE})
+set_property(TARGET boost-system PROPERTY 
+	IMPORTED_LOCATION_DEBUG ${Boost_SYSTEM_LIBRARY_DEBUG})
+
+add_library(boost-chrono STATIC IMPORTED)
+set_property(TARGET boost-chrono PROPERTY
+    INTERFACE_INCLUDE_DIRECTORIES ${Boost_INCLUDE_DIRS})
+set_property(TARGET boost-chrono PROPERTY 
+	IMPORTED_LOCATION_RELEASE ${Boost_CHRONO_LIBRARY_RELEASE})
+set_property(TARGET boost-chrono PROPERTY 
+	IMPORTED_LOCATION_DEBUG ${Boost_CHRONO_LIBRARY_DEBUG})
+
+add_library(boost-iostreams STATIC IMPORTED)
+set_property(TARGET boost-iostreams PROPERTY
+    INTERFACE_INCLUDE_DIRECTORIES ${Boost_INCLUDE_DIRS})
+set_property(TARGET boost-iostreams PROPERTY 
+	IMPORTED_LOCATION_RELEASE ${Boost_IOSTREAMS_LIBRARY_RELEASE})
+set_property(TARGET boost-iostreams PROPERTY 
+	IMPORTED_LOCATION_DEBUG ${Boost_IOSTREAMS_LIBRARY_DEBUG})
+
+add_library(boost-serialization STATIC IMPORTED)
+set_property(TARGET boost-serialization PROPERTY
+    INTERFACE_INCLUDE_DIRECTORIES ${Boost_INCLUDE_DIRS})
+set_property(TARGET boost-serialization PROPERTY 
+	IMPORTED_LOCATION_RELEASE ${Boost_SERIALIZATION_LIBRARY_RELEASE})
+set_property(TARGET boost-serialization PROPERTY 
+	IMPORTED_LOCATION_DEBUG ${Boost_SERIALIZATION_LIBRARY_DEBUG})
+
+add_library(boost-thread STATIC IMPORTED)
+set_property(TARGET boost-thread PROPERTY
+    INTERFACE_INCLUDE_DIRECTORIES ${Boost_INCLUDE_DIRS})
+set_property(TARGET boost-thread PROPERTY 
+	IMPORTED_LOCATION_RELEASE ${Boost_THREAD_LIBRARY_RELEASE})
+set_property(TARGET boost-thread PROPERTY 
+	IMPORTED_LOCATION_DEBUG ${Boost_THREAD_LIBRARY_DEBUG})
